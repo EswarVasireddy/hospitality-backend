b83c19a0d853d15f6aa492a615df3e27f5ce7f2a
EswarVasireddy <2300031194@kluniversity.in>
2026-09-07 10:22:52 -0400

    Create HospitalityApplication.java

---
+++ src/main/java/com/klu/hospitality/HospitalityApplication.java
@@ -0,0 +1,12 @@
+package com.klu.hospitality;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HospitalityApplication.class, args);
+    }
+}
